687dc8f4ce019ddfddf133f200a62bb895caddb8
csc5nz <csc5nz@virginia.edu>
2020-04-16 22:13:41 -0400

    create models for bill of sale

--- fastform/settings.py
+++ fastform/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'build.apps.BuildConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
