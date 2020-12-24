5893e72153f04a74af69be62c3e8d129a7369697
csc5nz <csc5nz@virginia.edu>
2020-12-23 22:35:13 -0500

    fixed iframe

--- fastform/create/urls.py
+++ fastform/create/urls.py
@@ -7,4 +7,5 @@ from . import views
 app_name = 'create'
 urlpatterns = [
     path('bill_of_sale', views.bill_of_sale, name='bill_of_sale'),
+    path('view_bill_of_sale', views.view_bill_of_sale, name='view_bill_of_sale'),
 ]
--- fastform/create/views.py
+++ fastform/create/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.template import loader
+from django.views.decorators.clickjacking import xframe_options_exempt
+from django.views.decorators.clickjacking import xframe_options_sameorigin
 
 # Create your views here.
+
 def bill_of_sale(request):
     template = loader.get_template('create/build_bill_of_sale_01.html')
+    context = {}
+
+    return HttpResponse(template.render(context, request))
+
+@xframe_options_exempt
+def view_bill_of_sale(request):
+    template = loader.get_template('create/bill_of_sale.html')
     context = {}
 
     return HttpResponse(template.render(context, request))
