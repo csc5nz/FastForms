7f9ce0468206ea4878648ac34fab9492271565f4
csc5nz <csc5nz@virginia.edu>
2020-12-31 20:42:44 -0500

    added three pages to bill of sale

--- create/forms.py
+++ create/forms.py
@@ -4,7 +4,6 @@ from .models import BillOfSale
 from django.utils import timezone
 
 class BillOfSaleForm(ModelForm):
-    
     class Meta:
         model = BillOfSale
         fields = ['seller_name', 'seller_address', 'seller_city',
@@ -13,11 +12,22 @@ class BillOfSaleForm(ModelForm):
 
 
 class BillOfSaleForm01(ModelForm):
-    
     class Meta:
         model = BillOfSale
         fields = ['seller_name', 'seller_address', 'seller_city',
         'seller_state', 'seller_zip']
 
+
+class BillOfSaleForm02(ModelForm):
+    class Meta:
+        model = BillOfSale
+        fields = ['buyer_name', 'buyer_address', 'buyer_city',
+        'buyer_state', 'buyer_zip']
+
+
+class BillOfSaleForm03(ModelForm):
+    class Meta:
+        model = BillOfSale
+        fields = ['properties', 'price', 'sale_date']
 
     
--- create/views.py
+++ create/views.py
@@ -1,37 +1,88 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from django.http import HttpResponse
 from django.template import loader
-from django.views.decorators.clickjacking import xframe_options_exempt
 from django.views.decorators.clickjacking import xframe_options_sameorigin
 from django.utils import timezone
 
 from .models import BillOfSale
-from .forms import BillOfSaleForm, BillOfSaleForm01
+from .forms import BillOfSaleForm, BillOfSaleForm01, BillOfSaleForm02, BillOfSaleForm03
 
 # Create your views here.
 
-def create_bill_of_sale(request):
-    template = loader.get_template('create/build_bill_of_sale_01.html')
-    context = {}
-
-    return HttpResponse(template.render(context, request))
-
-def create_bill_of_sale_01(request):
-    print('hello')
+def create_bill_of_sale(request, page, id=None):
+    instance = get_object_or_404(BillOfSale, id=id) if id else None
+    next_page = 0
     if request.method == "POST":
-        form = BillOfSaleForm01(request.POST)
-        print(form.errors)
+        if page == 1:
+            form = BillOfSaleForm01(request.POST, instance=instance)
+            next_page = 2
+        elif page == 2 and id:
+            form = BillOfSaleForm02(request.POST, instance=instance)
+            next_page = 3
+        elif page == 3 and id:
+            form = BillOfSaleForm03(request.POST, instance=instance)
+        else:
+            raise Http404("Page does not exist")
+
         if form.is_valid():
-            print('form is valid')
-            new_bill_of_sale = form.save()  
+            bill_of_sale = form.save()  
+        else:
+            print('not valid')
+        if page == 3:    
+            return redirect('index:home')    
+        return redirect('create:create_bill_of_sale', page=next_page, id=bill_of_sale.id)
+    
+    else:
+        if page == 1:
+            form = BillOfSaleForm01(instance=instance)
+        elif page == 2 and id:
+            form = BillOfSaleForm02(instance=instance)
+        elif page == 3 and id:
+            form = BillOfSaleForm03(instance=instance)
+        else:
+            raise Http404("Page does not exist")
+
+    return render(request, 'create/build_bill_of_sale_01.html', {'form': form, 'page': '01'})
+
+
+def create_bill_of_sale_01(request, id=None):
+    instance = get_object_or_404(BillOfSale, id=id) if id else None
+    if request.method == "POST":
+        form = BillOfSaleForm01(request.POST, instance=instance)
+        if form.is_valid():
+            bill_of_sale = form.save()  
+        else:
+            print('not valid')
+        return redirect('create:create_bill_of_sale_02', id=bill_of_sale.id)
+    else:
+        form = BillOfSaleForm01(instance=instance)
+    return render(request, 'create/build_bill_of_sale_01.html', {'form': form, 'page': '01'})
+
+def create_bill_of_sale_02(request, id=None):
+    instance = get_object_or_404(BillOfSale, id=id)
+    if request.method == "POST":
+        form = BillOfSaleForm02(request.POST, instance=instance)
+        if form.is_valid():
+            bill_of_sale = form.save()  
+        else:
+            print('not valid')
+        return redirect('create:create_bill_of_sale_03', id=bill_of_sale.id)
+    else:
+        form = BillOfSaleForm02(instance=instance)
+    return render(request, 'create/build_bill_of_sale_01.html', {'form': form, 'page': '01'})
+
+def create_bill_of_sale_03(request, id=None):
+    instance = get_object_or_404(BillOfSale, id=id)
+    if request.method == "POST":
+        form = BillOfSaleForm03(request.POST, instance=instance)
+        if form.is_valid():
+            bill_of_sale = form.save()  
         else:
             print('not valid')
         return redirect('index:home')
     else:
-        form = BillOfSaleForm01()
-        print('get, form')
-        print(form)
+        form = BillOfSaleForm03(instance=instance)
     return render(request, 'create/build_bill_of_sale_01.html', {'form': form, 'page': '01'})
 
 @xframe_options_sameorigin
