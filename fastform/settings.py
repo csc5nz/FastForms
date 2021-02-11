d126233f042bb1440732ead4e7f3dde7ab5e19b8
csc5nz <csc5nz@virginia.edu>
2021-02-10 22:25:10 -0500

    update allowed_host for heroku

--- fastform/settings.py
+++ fastform/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = secrete_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fastform.herokuapp.com']
 
 
 # Application definition
