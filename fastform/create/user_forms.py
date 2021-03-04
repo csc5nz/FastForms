0bd3138163d3f2f9cce81aa2dba482071d355857
csc5nz <csc5nz@virginia.edu>
2021-03-03 21:42:25 -0500

    added user log in and sign up

---
+++ fastform/create/user_forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.forms import ModelForm, CharField
+# from .models import BillOfSale
+# from django.utils import timezone
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+class LogInForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["username", "password"]
--- fastform/create/user_views.py
+++ fastform/create/user_views.py
@@ -3,15 +3,39 @@ from django.shortcuts import render, redirect, get_object_or_404, Http404
 from django.http import HttpResponse
 from django.template import loader
 # from django.views.decorators.clickjacking import xframe_options_sameorigin
-from django.utils import timezone
+# from django.utils import timezone
+from django.contrib.auth import authenticate, login, logout
 
 # from .models import BillOfSale, Document
-# from .forms import BillOfSaleForm, BillOfSaleForm01, BillOfSaleForm02, BillOfSaleForm03
+from .user_forms import SignUpForm, LogInForm
 
 
 def sign_up(request):
-    return render(request, 'create/sign_up.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('create:my_documents')
+    else:
+        form = SignUpForm()
+        print(form)
+    return render(request, 'create/sign_up.html', {'form': form})
 
 
 def log_in(request):
-    return render(request, 'create/log_in.html', {})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('create:my_documents')
+        else:
+            return HttpResponse("Invalid login ingormation.")
+    else:
+        form = LogInForm()
+    return render(request, 'create/log_in.html', {'form': form})
